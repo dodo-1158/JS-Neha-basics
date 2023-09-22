e426ce36bd021d73645731d2c6ebe5622b1a1e98
dodo-1158 <139553411+dodo-1158@users.noreply.github.com>
2023-09-22 17:10:49 +0000

    Basics about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID= 144553
+let accountEmail="dodo@gmail.com"
+var accountPassword= "12345"
+
+accountCity= "Jaipur"
+let accountState;
+
+// accountID=2 Not allowed because we can't change it as it is assigned as CONST in the beginning
+
+accountEmail="umlauts&digger@gmail.com"
+accountPassword="98765"
+accountCity="Mumbai"
+
+console.log(accountID);
+console.table([ accountID, accountEmail, accountCity, accountPassword, accountState])
