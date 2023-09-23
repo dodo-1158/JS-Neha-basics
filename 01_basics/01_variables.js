dd46e05e15883d33ea25f6b2b0bdc0dbdb3e3be2
dodo-1158 <139553411+dodo-1158@users.noreply.github.com>
2023-09-23 17:22:53 +0000

    Variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -7,6 +7,9 @@ let accountState;
 
 // accountID=2 Not allowed because we can't change it as it is assigned as CONST in the beginning
 
+/* Prefer not to use var because of issue in block scope and functional scope
+*/
+
 accountEmail="umlauts&digger@gmail.com"
 accountPassword="98765"
 accountCity="Mumbai"
