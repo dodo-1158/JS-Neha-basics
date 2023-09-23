2114f29510a37ad1d1840d72de1670290238ac41
dodo-1158 <139553411+dodo-1158@users.noreply.github.com>
2023-09-23 17:44:57 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //Treats all JS code in this file as Newer Version of JS which includes classes, modules etc.
+
+// alert(3+3)   this will not work here beacuse we are using nodejs as our runtime here, not a browser
+
+let name="Neha"   //string datatype
+let age=18        // number datatype
+let isloogedin= true  //boolean datatype
+let state= null  //null datatype
+
+//PRIMITIVE DATATYPES
+//number => 2^53
+//bigint
+//string => ""
+//boolean=> true/false
+//null => standalone value
+//undefined => when value is not defined 
+//symbol => unique  ---will be used in react 
+
+//object
+console.log(typeof isloogedin)  //Output is BOOLEAN
+console.log(typeof null)        //Output is OBJECT
+console.log(typeof undefined)   //Output is UNDEFINED
